04f3a439e447fbae5cdea1469ec9fc4ad5dd18a2
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-05-18 21:15:25 -0300

    Update profile and aliases

---
+++ install.ps1
@@ -0,0 +1,39 @@
+function Install() {
+  $baseProfile = "${env:HOMEDRIVE}${env:HOMEPATH}\profile.ps1";
+  Remove-Item $baseProfile;
+  New-Item -Path $baseProfile -ItemType SymbolicLink -Value ".\windows\profile.ps1";
+
+  $docs = [Environment]::GetFolderPath("MyDocuments");
+  $docs = (Join-Path $docs "WindowsPowerShell");
+
+  $powershellProfile = (Join-Path $docs "Microsoft.PowerShell_profile.ps1");
+  $powershellISEProfile = (Join-Path $docs "Microsoft.PowerShellISE_profile.ps1");
+
+  Remove-Item $powershellProfile;
+  Remove-Item $powershellISEProfile;
+  New-Item -Path $powershellProfile -ItemType SymbolicLink -Value ".\windows\Microsoft.PowerShell_profile.ps1";
+  New-Item -Path $powershellISEProfile -ItemType SymbolicLink -Value ".\windows\Microsoft.PowerShellISE_profile.ps1";
+
+  if (!(Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases")) {
+    mkdir "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\";
+  }
+
+  if (!(Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\dotfiles")) {
+    New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\dotfiles" -ItemType SymbolicLink -Value ".\windows\aliases";
+  }
+
+  if (!(Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\.gitconfig")) {
+    New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\.gitconfig" -ItemType SymbolicLink -Value ".\common\.gitconfig";
+  }
+
+  Write-Host ""
+  Write-Host "Base profile linked to $baseProfile";
+  Write-Host "Powershell profile linked to $powershellProfile";
+  Write-Host "Powershell ISE profile linked to $powershellISEProfile";
+  Write-Host "Aliases linked to ~/aliases/dotfiles";
+  Write-Host "Git config linked to ~/aliases/dotfiles";
+  Write-Host ""
+  Write-Host -ForegroundColor Green "Successfully installed!"
+}
+
+Install
--- windows/aliases/docker.ps1
+++ windows/aliases/docker.ps1
@@ -1,56 +1,77 @@
-Function c { docker-compose $args }
-Function cb { docker-compose build $args }
-Function cup { docker-compose up $args }
-Function cr { docker-compose run -d --service-ports --rm $args }
-Function crl { docker-compose run -d --service-ports --rm local $args }
-Function crd { docker-compose run -d --service-ports --rm develop $args }
-Function crprod { docker-compose run -d --service-ports --rm production $args }
-Function crt { docker-compose run -d --rm test $args }
-Function crp { docker-compose run -d --rm provision $args }
-Function crci { docker-compose run -d --rm ci $args }
-Function cps { docker-compose ps $args }
-Function clogs { docker-compose logs $args }
+function c { docker-compose $args }
+function cb { docker-compose build $args }
+function cup { docker-compose up $args }
+function cr { docker-compose run --service-ports --rm $args }
+function crl { docker-compose run --service-ports --rm local $args }
+function crd { docker-compose run --service-ports --rm develop $args }
+function crb { docker-compose run --service-ports --rm build $args }
+function crt { docker-compose run --rm test $args }
+function crp { docker-compose run --rm provision $args }
+function crprod { docker-compose run --rm production $args }
+function crci { docker-compose run --rm ci $args }
+function cru { docker-compose run --rm update $args }
+function cps { docker-compose ps $args }
+function clogs { docker-compose logs $args }
 
-Function docker-cleanup() {
-  docker rmi $(docker images -q -f dangling=true)
+function cinstall {
+  $projects = Get-ChildItem -Directory
+
+  for ($i = 0; $i -lt $projects.Count; $i++) {
+    $project = $projects[$i]
+
+    cd $project.Name
+    docker-compose run -d --rm test ONVAULT npm i
+    cd ..
+  }
+}
+
+function cbuild() {
+}
+
+function docker-cleanup() {
   docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes #--dry-run
 }
 
-Function docker-reset {
+function docker-reset {
   docker stop $(docker ps -q)
   docker rm $(docker ps -aq)
 }
 
-Function docker-reset-force() {
+function docker-reset-force() {
   docker rm -f $(docker ps -aq)
 }
 
-Function docker-proxy {
+function docker-proxy {
   docker run --privileged=true -d --name dockito-proxy -v /data/dockito-proxy/certs:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock -p 80:80 -p 443:443 dockito/proxy:latest
 }
 
-Function docker-vault {
-  docker run -d --name dockito-vault -p 14242:3000 -v C:/Users/bruno/.ssh:/vault/.ssh dockito/vault
+function docker-vault {
+  $sshfolder = "${env:HomeDrive}${env:HomePath}\.ssh\bravi"
+  docker run -d --name dockito-vault -p 14242:3000 -v ${sshfolder}:/vault/.ssh dockito/vault
 }
 
-Function docker-up {
+function docker-etcd2 {
+  docker run -d -ti --name etcd2 -p 4001:4001 -p 2379:2379 quay.io/coreos/etcd:v2.1.2 -name devbox -advertise-client-urls "http://10.0.75.2:2379,http://10.0.75.2:4001" -listen-client-urls "http://0.0.0.0:2379,http://0.0.0.0:4001" -debug
+}
+
+function docker-up {
   docker-reset
   docker-proxy
   docker-vault
+  docker-etcd2
+}
+function dup() {
+  docker-up
 }
 
-Function docker-up-force() {
+function docker-up-force() {
   docker-reset-force
   docker-up
 }
-
-Function dup() {
-  docker-up
-}
-
-Function dup-force() {
+function dup-force() {
   docker-up-force
 }
+function dupf { docker-up-force }
 
 function dreset() {
   docker-reset
@@ -60,7 +81,7 @@ function dreset-force() {
   docker-reset-force
 }
 
-Function dps() {
+function dps() {
   $NAME = $args[0]
 
   if ($NAME -and $NAME.ToString().StartsWith("-")) {
@@ -81,19 +102,19 @@ Function dps() {
     docker ps --filter "status=running" --filter "name=$NAME" --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'
   }
 }
-Function dpsi() {
+function dpsi() {
   docker ps --filter "name=$args" --filter status=running --format '{{.ID}} {{.Names}} ---> {{.Status}}\n  {{.Image}}\n  {{.Mounts}}\n  {{.Command}}\n  {{.Ports}}\n'
 }
 
-Function dps-exited() {
+function dps-exited() {
   dps $args -e
 }
 
-Function dps-all() {
+function dps-all() {
   dps $args -a
 }
 
-Function dbash() {
+function dbash() {
   $NAME = $args[0]
 
   echo ""
@@ -101,7 +122,7 @@ Function dbash() {
   docker exec -it $(docker ps -q --filter "name=$NAME" | Sort-Object -Descending | Select-Object -first 1) bash
 }
 
-Function dlogs() {
+function dlogs() {
   $NAME = $args[0]
   $PARAMS = $args | select -Skip 1
 
@@ -110,24 +131,24 @@ Function dlogs() {
   docker logs $PARAMS $(docker ps -aq --filter "name=$NAME" | Sort-Object -Descending | Select-Object -first 1)
 }
 
-Function dstart() {
+function dstart() {
   $NAME = $args[0]
 
   docker start $(docker ps -aq --filter "name=$NAME" | Sort-Object -Descending | Select-Object -first 1)
 }
 
-Function drestart() {
+function drestart() {
   $NAME = $args[0]
 
   docker restart $(docker ps -aq --filter "name=$NAME" | Sort-Object -Descending | Select-Object -first 1)
 }
 
-Function dstop() {
+function dstop() {
   $NAME = $args[0]
 
   docker stop $(docker ps -aq --filter "name=$NAME" | Sort-Object -Descending | Select-Object -first 1)
 }
 
-Function dre() {
+function dre() {
   docker rm -f $(docker ps -q --filter status=exited)
 }
