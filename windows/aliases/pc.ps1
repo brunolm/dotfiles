4571f0ce479fb1fc8b86106638f0a9984bd00f6a
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-06-10 20:50:23 -0300

    Update

--- windows/aliases/pc.ps1
+++ windows/aliases/pc.ps1
@@ -1,10 +1,21 @@
 function Disable-Beep() {
-  Set-PSReadlineOption -BellStyle None
-  # set-service beep -startuptype disabled
+    Set-PSReadlineOption -BellStyle None
+    # set-service beep -startuptype disabled
 }
 
 function Start-Powershell() {
-  Start-Process powershell -Verb runAs
+    Start-Process powershell -Verb runAs
+}
+
+function psl() {
+    $saveY = [console]::CursorTop
+    $saveX = [console]::CursorLeft
+
+    while ($true) {
+        Get-Process | Sort-Object -Descending CPU | Select-Object -First 30;
+        Start-Sleep -Seconds 2;
+        [console]::setcursorposition($saveX,$saveY+3)
+    }
 }
 
 Disable-Beep
