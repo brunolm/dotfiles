9ebf061d3c157e294f27ffeb2c2d575f93bfb3be
Bruno Leonardo Michels <brunolm7@gmail.com>
2017-08-17 07:59:38 -0300

    Update nvm add auto-complete

--- windows/aliases/nvm/node.ps1
+++ windows/aliases/nvm/node.ps1
@@ -1,28 +1,40 @@
-$env:NODE_DIR="C:\Program Files\nodejs";
-$env:NODE_VERSIONS_DIR=(Join-Path $env:NODE_DIR "versions");
+$env:NODE_DIR = "C:\Program Files\nodejs";
+$env:NODE_VERSIONS_DIR = (Join-Path $env:NODE_DIR "versions");
 
-function nvm($command, [Switch] $help) {
-    $commandPath = "$PSScriptRoot\commands\$command.ps1";
+function nvm(
+    [ValidateSet(
+        "deactivate",
+        "default",
+        "install",
+        "ls-remote",
+        "ls",
+        "use"
+    )]
+    $command,
+    [Alias("h")][Switch]
+    $help
+) {
+    if ($command) {
+        $commandPath = "$PSScriptRoot\commands\$command.ps1";
 
-    if (!$command -or !(Test-Path $commandPath)) {
+        & "$commandPath" @args $(If ($help) {"--help"})
+    }
+    elseif ($help) {
         Write-Host "Usage:"
         Write-Host -ForegroundColor Green "  nvm <command> <args>"
 
         Write-Host ""
         Write-Host "Available commands"
 
-        foreach($_ in Get-ChildItem $PSScriptRoot\commands -Name) {
-            if ($_ -ne "_base.ps1") {
-                Write-Host -NoNewline "  "
-                [System.IO.Path]::GetFileNameWithoutExtension($_)
-            }
-        }
+        Write-Host "deactivate"
+        Write-Host "default"
+        Write-Host "install"
+        Write-Host "ls-remote"
+        Write-Host "ls"
+        Write-Host "use"
 
         Write-Host ""
         Write-Host "Use -h or --help for info on each command"
         Write-Host ""
-        return;
     }
-
-    & "$commandPath" @args $(If ($help) {"--help"})
 }
