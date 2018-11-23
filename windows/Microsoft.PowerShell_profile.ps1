9eb3135de7951dc452aa49b98ec65e8072941152
Bruno Leonardo Michels <brunolm7@gmail.com>
2018-11-23 16:21:12 -0200

    Stuff

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,9 @@ try {
 } catch { }
 
 . "${env:HomeDrive}${env:HomePath}\profile.ps1"
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
