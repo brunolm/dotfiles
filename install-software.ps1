48254dad75cf7a63f323dab9d4c13854f1ab0bc8
Bruno Leonardo Michels <brunolm7@gmail.com>
2021-03-04 15:18:20 -0300

    Add choco installs

---
+++ install-software.ps1
@@ -0,0 +1,11 @@
+function InstallSoftware() {
+  choco install powershell-core
+  choco install powertoys
+
+  choco install heroku-cli
+  choco install vercel
+  choco install nvs
+  choco install vim
+}
+
+InstallSoftware
