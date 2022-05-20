4193f694c2f00a503e175826858f014509c3a76d
Bruno Leonardo Michels <brunolm7@gmail.com>
2022-05-19 19:40:35 -0700

    Add NP

--- windows/aliases/np.ps1
+++ windows/aliases/np.ps1
@@ -1,6 +1,27 @@
 function NP() {
-  $e = Get-Content "./.editorconfig"
-  $p = Get-Content "./.prettierrc"
+  $e = @'
+root = true
+
+[*]
+end_of_line = lf
+insert_final_newline = true
+indent_style = space
+indent_size = 2
+trim_trailing_whitespace = true
+
+[windows/**]
+indent_size = 2
+'@
+
+  $p = @'
+{
+  "arrowParens": "always",
+  "printWidth": 120,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "semi": false
+}
+'@
 
   Out-File -FilePath ".editorconfig" -InputObject $e
   Out-File -FilePath ".prettierrc" -InputObject $p
