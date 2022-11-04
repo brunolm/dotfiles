de34cd45b1af7caf42b99b5ff8c024343b7c82f3
Bruno Leonardo Michels <brunolm7@gmail.com>
2022-11-04 11:31:25 -0300

    Add pyenv

--- install-software.ps1
+++ install-software.ps1
@@ -25,6 +25,9 @@ function InstallSoftware() {
   # yarn global add tree-cli
 
   # npm install --global opusfluxus
+
+  # pyenv pyenv-win
+  Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
 }
 
 InstallSoftware
