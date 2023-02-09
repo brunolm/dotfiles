e8ee393eaf499cad8a57b05b4a8ad0589cb2fdec
Bruno Leonardo Michels <brunolm7@gmail.com>
2023-02-09 11:50:53 -0300

    Update startup script

--- windows/startup/startup.ps1
+++ windows/startup/startup.ps1
@@ -1,1 +1,11 @@
-Get-ChildItem c:\system\startup | ForEach-Object { Start-Process $_.FullName }
+try {
+  Set-ExecutionPolicy RemoteSigned
+}
+catch {}
+
+try {
+  Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
+catch {}
+
+Get-ChildItem c:\system\startup | ForEach-Object { Start-Process $_.FullName -verb runas }
