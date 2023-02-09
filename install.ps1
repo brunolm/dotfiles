7fccf0b55bc15d44dcc83e112f177d559c00583e
Bruno Leonardo Michels <brunolm7@gmail.com>
2023-02-09 11:29:18 -0300

    Fix

--- install.ps1
+++ install.ps1
@@ -1,8 +1,8 @@
 function Install() {
   $baseProfile = "${env:HOMEDRIVE}${env:HOMEPATH}\profile.ps1";
   Remove-Item $baseProfile;
-  New-Item -Path $baseProfile -ItemType SymbolicLink -Value ".\windows\profile.ps1";
-  New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\env.ps1" -ItemType SymbolicLink -Value ".\windows\env.ps1";
+  New-Item -Path $baseProfile -ItemType SymbolicLink -Value (Resolve-Path ".\windows\profile.ps1");
+  New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\env.ps1" -ItemType SymbolicLink -Value (Resolve-Path ".\windows\env.ps1");
 
   $docs = [Environment]::GetFolderPath("MyDocuments");
   $docs = (Join-Path $docs "WindowsPowerShell");
@@ -12,24 +12,24 @@ function Install() {
 
   Remove-Item $powershellProfile;
   Remove-Item $powershellISEProfile;
-  New-Item -Path $powershellProfile -ItemType SymbolicLink -Value ".\windows\Microsoft.PowerShell_profile.ps1";
-  New-Item -Path $powershellISEProfile -ItemType SymbolicLink -Value ".\windows\Microsoft.PowerShellISE_profile.ps1";
+  New-Item -Path $powershellProfile -ItemType SymbolicLink -Value (Resolve-Path ".\windows\Microsoft.PowerShell_profile.ps1");
+  New-Item -Path $powershellISEProfile -ItemType SymbolicLink -Value (Resolve-Path ".\windows\Microsoft.PowerShellISE_profile.ps1");
 
   if (!(Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases")) {
     mkdir "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\";
   }
 
   if (!(Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\dotfiles")) {
-    New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\dotfiles" -ItemType SymbolicLink -Value ".\windows\aliases";
+    New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\aliases\dotfiles" -ItemType SymbolicLink -Value (Resolve-Path ".\windows\aliases");
   }
 
   if (!(Test-Path "${env:HOMEDRIVE}${env:HOMEPATH}\.gitconfig")) {
-    New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\.gitconfig" -ItemType SymbolicLink -Value ".\common\.gitconfig";
+    New-Item -Path "${env:HOMEDRIVE}${env:HOMEPATH}\.gitconfig" -ItemType SymbolicLink -Value (Resolve-Path ".\common\.gitconfig");
   }
 
   # New-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" -ItemType SymbolicLink -Value ".\windows\startup";
-  Copy-Item -Path ".\windows\startup\startup.cmd" -Destination "C:\System"
-  Copy-Item -Path ".\windows\startup\startup.ps1" -Destination "C:\System"
+  Copy-Item -Path (Resolve-Path ".\windows\startup\startup.cmd") -Destination "C:\System"
+  Copy-Item -Path (Resolve-Path ".\windows\startup\startup.ps1") -Destination "C:\System"
 
   New-Item -Path "C:\System\Startup" -ItemType SymbolicLink -Value (Resolve-Path ".\windows\startup-files\");
 
