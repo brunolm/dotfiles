437acd3f606c6ba146c77c8cb78cb0f90f1a503e
Bruno Leonardo Michels <brunolm7@gmail.com>
2025-04-02 12:35:15 -0300

    git

--- windows/aliases/git.psm1
+++ windows/aliases/git.psm1
@@ -1,15 +1,19 @@
-function Clear-GitBranches() {
-    git fetch --all --prune
-    git branch -vv `
-        | Where-Object { $_ -like '*origin/*: gone*' } `
-        | Select-Object -Property @{N='Name';E={($_ -split ' ')[2]}} `
-        | ForEach-Object { git branch -d $_.Name }
+function Git-ClearLocalGoneBranches() {
+  git fetch --all --prune
+  git branch -vv `
+  | Where-Object { $_ -like '*origin/*: gone*' } `
+  | Select-Object -Property @{N = 'Name'; E = { ($_ -split ' ')[2] } } `
+  | ForEach-Object { git branch -d $_.Name }
 }
 
-function Update-Development() {
-    git fetch --all --prune
-    git checkout development
-    git pull
+function Git-UpdateDevelop() {
+  git fetch --all --prune
+  git checkout develop
+  git pull
+}
+
+function Git-ClearLocalMergedBranches() {
+  git branch --merged | Where-Object { $_ -notmatch "main|develop" } | ForEach-Object { git branch -d $_.Trim() }
 }
 
 Export-ModuleMember -Function "*"
