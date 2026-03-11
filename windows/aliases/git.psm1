7f0855b7ba6c8055fd34e614014c8321ab2b21a0
Bruno Leonardo Michels <brunolm7@gmail.com>
2026-03-11 09:12:23 -0300

    Improve git

--- windows/aliases/git.psm1
+++ windows/aliases/git.psm1
@@ -28,5 +28,44 @@ function Git-GPGReload() {
   gpg-connect-agent reloadagent /bye
 }
 
+function Git-DeleteBranches() {
+  $branches = git branch --format='%(refname:short)' | ForEach-Object { $_.Trim() }
+  $picks = Multi-Select -Items $branches
+  if ($picks) {
+    Write-Host "Branches to delete:"
+    $picks | ForEach-Object { Write-Host "  - $_" }
+    $confirm = Read-Host "Confirm? (Y/n)"
+    if ($confirm -eq '' -or $confirm -eq 'Y' -or $confirm -eq 'y') {
+      $picks | ForEach-Object { git branch -D $_ }
+    }
+    else {
+      Write-Host "Cancelled."
+    }
+  }
+  else {
+    Write-Host "No branches selected."
+  }
+}
+
+function Git-Squash() {
+  $log = git log --oneline -15
+  if (-not $log) {
+    Write-Host "No commits found."
+    return
+  }
+  $selected = Single-Select -Items $log
+  if ($selected) {
+    $hash = ($selected -split ' ')[0]
+    Write-Host "Will reset --soft to: $selected"
+    $confirm = Read-Host "Confirm? (Y/n)"
+    if ($confirm -eq '' -or $confirm -eq 'Y' -or $confirm -eq 'y') {
+      git reset --soft $hash
+    }
+    else {
+      Write-Host "Cancelled."
+    }
+  }
+}
+
 Export-ModuleMember -Function "*"
 Export-ModuleMember -Alias "*"
--- windows/aliases/terminal.ps1
+++ windows/aliases/terminal.ps1
@@ -5,3 +5,78 @@ function rm-rf {
   )
   Remove-Item -Path $Path -Recurse -Force
 }
+
+function Multi-Select {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string[]]$Items
+  )
+
+  $selected = [bool[]]::new($Items.Count)
+  $pos = 0
+
+  function Render {
+    [Console]::SetCursorPosition(0, $startY)
+    for ($i = 0; $i -lt $Items.Count; $i++) {
+      $check = if ($selected[$i]) { 'x' } else { ' ' }
+      $prefix = if ($i -eq $pos) { '>' } else { ' ' }
+      Write-Host "$prefix [$check] $($Items[$i])                    "
+    }
+    Write-Host "`nUse Up/Down to move, Space to toggle, Enter to confirm" -NoNewline
+  }
+
+  $startY = [Console]::CursorTop
+  Render
+
+  while ($true) {
+    $key = [Console]::ReadKey($true)
+    switch ($key.Key) {
+      'UpArrow' { if ($pos -gt 0) { $pos-- } }
+      'DownArrow' { if ($pos -lt $Items.Count - 1) { $pos++ } }
+      'Spacebar' { $selected[$pos] = !$selected[$pos] }
+      'Enter' { break }
+    }
+    if ($key.Key -eq 'Enter') { break }
+    Render
+  }
+
+  Write-Host "`n"
+  $picks = for ($i = 0; $i -lt $Items.Count; $i++) { if ($selected[$i]) { $Items[$i] } }
+  return $picks
+}
+
+function Single-Select {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string[]]$Items
+  )
+
+  $pos = 0
+
+  function Render {
+    [Console]::SetCursorPosition(0, $startY)
+    for ($i = 0; $i -lt $Items.Count; $i++) {
+      $prefix = if ($i -eq $pos) { '>' } else { ' ' }
+      Write-Host "$prefix $($Items[$i])                    "
+    }
+    Write-Host "`nUse Up/Down to move, Enter to confirm" -NoNewline
+  }
+
+  $startY = [Console]::CursorTop
+  Render
+
+  while ($true) {
+    $key = [Console]::ReadKey($true)
+    switch ($key.Key) {
+      'UpArrow' { if ($pos -gt 0) { $pos-- } }
+      'DownArrow' { if ($pos -lt $Items.Count - 1) { $pos++ } }
+      'Enter' { break }
+    }
+    if ($key.Key -eq 'Enter') { break }
+    Render
+  }
+
+  Write-Host "`n"
+  return $Items[$pos]
+}
+
