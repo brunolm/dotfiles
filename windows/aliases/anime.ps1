496c04d8cc2b26b7d758985d5e3ca726fec50376
Bruno Leonardo Michels <brunolm7@gmail.com>
2026-09-03 20:34:45 -0300

    chore(aliases): remove anime and azure aliases

--- windows/aliases/anime.ps1
+++
@@ -1,5 +0,0 @@
-function anime() {
-  cd C:\BrunoLM\Projects\animewatch
-  start http://localhost:3137
-  bun start
-}
--- windows/aliases/azure.ps1
+++
@@ -1,3 +0,0 @@
-function AZ-PROpen($id, $title, $branch) {
-  az repos pr create --title $title --work-items $id -t $branch
-}
